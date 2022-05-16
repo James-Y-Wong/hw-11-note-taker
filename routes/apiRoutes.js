8267754e1f5df13008484b82189a05684214eea3
James-Y-Wong <jamesyeuwong@gmail.com>
2022-05-16 18:43:27 -0400

    added get and post to apiroutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,13 +7,23 @@ const {
   } = require('../helpers/fsUtils');
 
 router.get("/notes", (req, res) => {
-    res.json('Get!');
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 router.post("/notes", (req, res) => {
     const newNote = req.body;
-    newNote.id = uuidv4()
+    newNote.id = uuidv4();
+
     readAndAppend(newNote, "./db/db.json");
+    res.json(newNote);
+});
+
+router.post("/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    
+    readAndAppend(newNote, "./db/db.json");
+    res.json(newNote);
 });
 
 module.exports = router;
