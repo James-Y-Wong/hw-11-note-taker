56659619e7c6be76db7d8af02a57a8a3a6b5b630
James-Y-Wong <jamesyeuwong@gmail.com>
2022-05-16 18:30:37 -0400

    going a different route and using something similar to mini project

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
-const store = require("../db/store");
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
 
 router.get("/notes", (req, res) => {
     res.json('Get!');
 });
 
 router.post("/notes", (req, res) => {
-    store.addNote(req.body);
-    res.json('Post!');
+    const newNote = req.body;
+    newNote.id = uuidv4()
+    readAndAppend(newNote, "./db/db.json");
 });
 
 module.exports = router;
